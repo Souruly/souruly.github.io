8d33bb3a43209bcd6b69834c0aef2af2a09d2bb4
Souruly <souruly@gmail.com>
2017-06-29 19:18:40 +0530

    Add files via upload

---
+++ cell2.js
@@ -0,0 +1,28 @@
+function Cell(x,y)
+{
+   this.x = x;
+   this.y = y;
+   this.weight = ' ';
+   this.alreadySet = false;
+   this.backgroundColor = 255;
+   // this.isWord = true;
+
+   this.setBackgroundColor = function(r,g,b)
+   {
+      var x = color(r,g,b);
+      this.backgroundColor = x;
+   }
+
+   this.show = function()
+   {
+      this.fillcolor = 255;
+      stroke(0);
+      strokeWeight(0.1);
+      fill(this.backgroundColor);
+      rect(this.x*w,this.y*w,w,w);
+      textSize(8*w/10);
+      textAlign(CENTER);
+      fill(0);
+      text(this.weight , w/2+this.x*w , w+this.y*w-w/6);
+   }
+}
