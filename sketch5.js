6cbaa412c1640188b218d1c6f4e3beefe11deb82
Souruly <souruly@gmail.com>
2017-06-30 20:45:59 +0530

    Add files via upload

---
+++ sketch5.js
@@ -0,0 +1,274 @@
+var data = [];
+var grid = [];
+var count = 8;
+var gotData = false;
+var w = 30;
+var csize = 600;
+var done = 0;
+var gridSize;
+var wordsFilled = false;
+var button1;
+var button2;
+var frameCount = 0;
+
+function getWords(words)
+{
+   var x = words.words.length
+   var i=0;
+   while(i<count)
+   {
+      var y = floor(random(x));
+      var z = words.words[y];
+      z = z.toUpperCase();
+      if(z.length<=10)
+      {
+         var inpw = new Word(z);
+         data.push(inpw);
+         i++;
+      }
+   }
+   console.log(data);
+   gotData = true;
+}
+
+function setup()
+{
+   createCanvas(800,csize);
+   frameRate(30);
+   loadJSON("words.json",getWords);
+   gridSize = csize / w;
+   gridSize = gridSize - 2;
+   for(var i = 1 ; i <= gridSize ; i++)
+   {
+      for(var j = 1 ; j<= gridSize ; j++)
+      {
+         var cell = new Cell(i,j);
+         grid.push(cell);
+      }
+   }
+
+   button1 = createButton('Solution');
+   button1.position(600,550);
+   console.log("Button1 Created");
+   button1.mousePressed(solveGame);
+   button2 = createButton('Hide Solution');
+   button2.position(670,550);
+   console.log("Button2 Created");
+   button2.mousePressed(hideSolution);
+}
+
+function draw()
+{
+   frameCount++;
+   background(240,240,240);
+   stroke(0);
+   strokeWeight(5);
+   rect(w,w,gridSize*w,gridSize*w);
+   noStroke(0);
+   // resetGrid();
+   if(gotData==true)
+   {
+      for(var i=0 ; i<data.length ; i++)
+      {
+         if(data[i].set==false)
+         {
+            var x = fillWordIntoGrid(data[i].word, floor(random(gridSize)) , floor(random(gridSize)) , floor(random(0,2)) , floor(random(0,2)), i);
+            if(x==1)
+            {
+               data[i].set=true;
+               done++;
+            }
+         }
+      }
+      // background(240);
+      // textSize(20);
+      for(var i=0 ; i<data.length ; i++)
+      {
+         textSize(20);
+         text(data[i].word,650,90+i*60);
+         // fillWordIntoGrid(data[i],0,i,0,1);
+      }
+      // fillGrid();
+
+      // for(var i=0 ; i<grid.length ; i++)
+      // {
+      //    grid[i].show();
+      // }
+      // if(wordsFilled==true)
+      // {
+      //     noLoop();
+      // }
+      if(done==count)
+      {
+         fillGrid();
+         for(var i=0 ; i<grid.length ; i++)
+         {
+            grid[i].show();
+         }
+         if(wordsFilled==false)
+         {
+            console.log(data);
+            console.log("STOP");
+            wordsFilled = true;
+         }
+      }
+      else
+      {
+         textSize(50);
+         text("Generating...",150,200);
+         textSize(20);
+         var refreshMessage = "Please consider refreshing the page if the counter goes above 10  --------->";
+         text(refreshMessage,150,250,250,350);
+         textSize(50);
+         text(round(frameCount/30),400,325);
+      }
+      // else
+      // {
+      //    done = 0;
+      // }
+   }
+   // else
+   // {
+   //    stroke(0);
+   //    // strokeWeight(5);
+   //    text(20);
+   //    text("Generating...", 5,5);
+   //    noStroke(0);
+   // }
+}
+
+function outOfBounds(r,c)
+{
+   var status;
+   if(r<0 || c<0 || r>=gridSize || c>=gridSize)
+   {
+      status = true;
+   }
+   else
+   {
+      status = false;
+   }
+   return status;
+}
+
+function index(r,c)
+{
+   if(r<0 || c<0 || r>=gridSize || c>=gridSize)
+   {
+      return -1;
+   }
+   else
+   {
+      return r+c*gridSize;
+   }
+}
+
+function fillWordIntoGrid(word,r,c,xDir,yDir,ii)
+{
+   if(wordSetFailure(word,r,c,xDir,yDir))
+   {
+      return 0;
+   }
+   else
+   {
+      console.log(r,c,xDir,yDir);
+      for(var i=0 ; i<word.length ; i++)
+      {
+         var rr = r+yDir*i;
+         var cc = c+xDir*i
+         var ll = index(rr,cc);
+         grid[ll].weight = word[i];
+         data[ii].locations.push(ll);
+         grid[ll].alreadySet = true;
+         grid[ll].isWord = true;
+      }
+      return 1;
+   }
+}
+
+function wordSetFailure(word,r,c,xDir,yDir)
+{
+   var l = word.length;
+   if(xDir==yDir)
+   {
+      return true;
+   }
+
+   if(outOfBounds(r+yDir*l,c+xDir*l))
+   {
+      return true;
+   }
+
+   for(var i=0 ; i<l ; i++)
+   {
+      if(grid[index(r+yDir*i,c+xDir*i)].alreadySet==true)
+      {
+         if(grid[index(r+yDir*i,c+xDir*i)].weight != word[i])
+         {
+            return true;
+         }
+      }
+   }
+   return false;
+}
+
+function getRandomCharacter()
+{
+   var x = round(random(65,90));
+   var y = String.fromCharCode(x);
+   return y;
+}
+
+function resetGrid()
+{
+   for(var i = 0 ; i < grid.length ; i++)
+   {
+      grid[i].weight = ' ';
+   }
+}
+
+function fillGrid()
+{
+   for(var i=0 ; i<grid.length ; i++)
+   {
+      if(grid[i].alreadySet==false)
+      {
+         grid[i].weight = getRandomCharacter();
+         grid[i].isWord = false;
+         grid[i].alreadySet=true;
+      }
+   }
+}
+
+function solveGame()
+{
+   if(wordsFilled==true)
+   {
+      for(var i=0 ; i<data.length ; i++)
+      {
+         var r = random(180);
+         var g = random(180);
+         var b = random(180);
+         for(var j=0 ; j<data[i].locations.length ; j++)
+         {
+            var l = data[i].locations[j];
+            grid[l].setBackgroundColor(r+10*j,g+10*j,b+10*j);
+         }
+      }
+   }
+}
+
+function hideSolution()
+{
+   if(wordsFilled==true)
+   {
+      for(var i=0 ; i<data.length ; i++)
+      {
+         for(var j=0 ; j<data[i].locations.length ; j++)
+         {
+            var l = data[i].locations[j];
+            grid[l].setBackgroundColor(255,255,255);
+         }
+      }
+   }
+}
