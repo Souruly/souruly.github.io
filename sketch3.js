6a2caecdb47f5bc962b23129c2e6e0202a87249d
Souruly <souruly@gmail.com>
2017-10-16 20:03:40 +0530

    Update sketch3.js

--- sketch3.js
+++ sketch3.js
@@ -57,7 +57,7 @@ function draw()
   /////
   textSize(40);
   textStyle(BOLD);
-  text("CAMOUGLAGE SIMULATION",900,0);
+  text("CAMOUFLAGE SIMULATION",900,0);
   textStyle(NORMAL);
   textSize(18);
   text("EVOLUTION, USING GENETIC ALGORITHMS",900,40);
