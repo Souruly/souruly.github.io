991cfc38f6bf895f991e6f91ab0eb3e985213cf2
Sourav Kulkarni <souruly@gmail.com>
2020-10-17 23:45:40 +0530

    Removed previous project

--- component---src-pages-blog-js-f9ebfc6b4356f2888449.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{vx99:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),c=n.n(a),r=n("Kvkj");t.default=function(){return c.a.createElement(r.b,null,c.a.createElement("h1",{className:"pt-5 text-center"},"Blog"))}}}]);
-//# sourceMappingURL=component---src-pages-blog-js-f9ebfc6b4356f2888449.js.map
