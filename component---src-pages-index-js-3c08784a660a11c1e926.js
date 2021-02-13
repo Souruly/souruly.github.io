2047b06da0419bc5c946b53f559afd62e674e990
Sourav Kulkarni <souruly@gmail.com>
2021-02-13 14:56:50 +0100

    Updates

--- component---src-pages-index-js-3c08784a660a11c1e926.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{RXBc:function(n,t,e){"use strict";e.r(t),e.d(t,"default",(function(){return a}));var r=e("q1tI"),u=e.n(r),i=e("Kvkj");function a(){return u.a.createElement(i.b,{bigTitle:"Sourav Kulkarni"})}}}]);
-//# sourceMappingURL=component---src-pages-index-js-3c08784a660a11c1e926.js.map
