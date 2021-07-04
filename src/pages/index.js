bed4faaf5c44f4876389ebdf7d18f3ac6d2fd0ae
Sourav Kulkarni <souruly@gmail.com>
2021-07-04 21:54:37 +0200

    Added quotes

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,8 +19,11 @@ import { Link } from "gatsby"
 export default function Home() {
   const quotes = ["“All the world's a stage, and all the men and women merely players: they have their exits and their entrances; and one man in his time plays many parts, his acts being seven ages.”", 
   "‘Tell me,’ the great twentieth-century philosopher Ludwig Wittgenstein once asked a friend, ‘why do people always say it was natural for man to assume that the sun went round the Earth rather than that the Earth was rotating?’ His friend replied, ‘Well, obviously because it just looks as though the Sun is going round the Earth.’ Wittgenstein responded, ‘Well, what would it have looked like if it had looked as though the Earth was rotating?’",
-  "“Now we come back to the 'Chicken and Egg Problem'. And much like the 'Chicken adn Egg Problem', the answer to this question is...'YES'.”",
-  "“Now I'm not sure, was I a human who just dreamt it was a butterfly or am I a butterfly who was dreaming it's a human?”"];
+  "“Now this is similar to the 'Chicken and Egg Problem'. And much like all 'Chicken and Egg' problems, the answer to this question is 'YES'.”",
+  "“Now I'm not sure, was I a human who just dreamt it was a butterfly or am I a butterfly who was dreaming it's a human?”",
+  "“By all means let's be open-minded, but not so open-minded that our brains drop out.”",
+  "“Four legs good, two legs bad.....Four legs good, two legs better!”",
+  "“If liberty means anything at all it means the right to tell people what they do not want to hear.”"];
 
   const quote = quotes[Math.floor(Math.random()*quotes.length)];
 
